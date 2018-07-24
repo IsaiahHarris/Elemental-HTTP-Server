6ec8d0bd88c756b7fc5170d5e215213e058ec264
Isaiah Harris <harrisfambam@gmail.com>
2018-07-23 15:38:09 -1000

    mvp

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const { parse } = require('querystring');
 
 //404 response
 
-function send404(response) {
-  response.writeHead(404, { 'Content-Type': 'text/plain' });
-  response.write('error 404: page not found');
-  response.end();
-}
+// function send404(response) {
+//   response.writeHead(404, { 'Content-Type': 'text/plain' });
+//   response.write('error 404: page not found');
+//   response.end();
+// }
 
 
 
@@ -17,19 +17,43 @@ http.createServer((request, response) => {
   let url = request.url;
   if (request.method === 'GET') {
 
-    if (!stats.isFile('./public/'+url)) {
-      response.writeHead(404,{ 'Content-Type': 'text/html' });
-      fs.createReadStream('./public/404.html').pipe(response);
-    } else {
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      fs.createReadStream('./public/' + url).pipe(response);
-    }
-  }else if (request.method === 'POST') {
+    fs.stat('./public/' + url, function (err, stat) {
+      if (err === null) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile(`./public` + url, (err, data) => {
+          if (err) {
+            console.log('error');
+          } else {
+            response.write(data.toString().trim())
+            response.end(() => {
+              console.log('request fulfilled');
+            });
+          }
+        })
+      } else if (err.code === 'ENOENT') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile(`./public` + url, (err, data) => {
+          if (err) {
+            console.log('error');
+          } else {
+            response.write(data.toString().trim())
+            response.end((() => {
+              console.log('request fulfilled');
+            }));
+          }
+        })
+      } else {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        fs.createReadStream('./public/404.html').pipe(response);
+      }
+    });
+
+  } else if (request.method === 'POST') {
     collectRequestData(request, result => {
 
-        let fileName = result.elName+'.html';
+      let fileName = result.elName + '.html';
 
-        let datas = `<!DOCTYPE html>
+      let datas = `<!DOCTYPE html>
         <html lang="en">
         <head>
           <meta charset="UTF-8">
@@ -44,42 +68,54 @@ http.createServer((request, response) => {
           <p><a href="/">back</a></p>
         </body>
         </html>`;
-        fs.readFile('./public/index.html', (err,data)=>{
-          if(err){
-            console.log('error');
-          }else {
-          }
-        })
-        fs.writeFile('./public/' + fileName, datas, (err)=>{
-          if(err){
-            console.log('error');
-          }else {
-            response.writeHead(200, { 'Content-Type': 'application/json', 'Content-Body': '{ "url" : true }'});
-            response.end();
-          }
-        }); 
+
+      fs.writeFile('./public/' + fileName, datas, (err) => {
+        if (err) {
+          console.log('error');
+        } else {
+          response.writeHead(200, { 'Content-Type': 'application/json', 'Content-Body': '{ "url" : true }' });
+          response.end();
+        }
+      });
+      fs.readFile('./public/index.html', (err, data)=>{
+        if (err){
+          console.log(err);
+        }else {
+         let indexFile =  data.toString().split("\n");
+          indexFile.splice(indexFile.length-5, 0,`
+        <li>
+          <a href="/${result.elName}.html">${result.elName}</a>
+        </li>`);
+          var text = indexFile.join("\n");
+          fs.writeFile('./public/index.html', text, function (err) {
+            if (err){
+              console.log(err);
+            } 
+          });
+        }
+      })
 
     });
-}
+  }
 
 
-}).listen(6969,'0.0.0.0', () => {
+}).listen(6969, '0.0.0.0', () => {
   console.log('server is listening to port 6969');
 });
 
 function collectRequestData(request, callback) {
   console.log(request.headers['content-type']);
   const FORM_URLENCODED = 'application/x-www-form-urlencoded';
-  if(request.headers['content-type'] === FORM_URLENCODED) {
-      let body = '';
-      request.on('data', chunk => {
-          body += chunk.toString();
-      });
-      request.on('end', () => {
-          callback(parse(body));
-      });
+  if (request.headers['content-type'] === FORM_URLENCODED) {
+    let body = '';
+    request.on('data', chunk => {
+      body += chunk.toString();
+    });
+    request.on('end', () => {
+      callback(parse(body));
+    });
   }
   else {
-      callback(null+ 'hi');
+    callback(null + 'hi');
   }
 }
